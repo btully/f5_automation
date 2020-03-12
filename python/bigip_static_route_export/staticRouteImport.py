3a82fa1c3a8c23daf1fd0a4207cd4e0b6819583f
Brian Tully <tully.brian@gmail.com>
2020-03-12 16:40:17 -0400

    adding scripts

--- python/bigip_static_route_export/staticRouteImport.py
+++ python/bigip_static_route_export/staticRouteImport.py
@@ -11,11 +11,11 @@ requests.packages.urllib3.disable_warnings()
 parser = argparse.ArgumentParser(description='F5 Big-IP iApp conversion utility')
 parser.add_argument('--host', help='BIG-IP IP or Hostname', required=True)
 parser.add_argument('--username', help='BIG-IP Username', required=True)
+parser.add_argument('--file', help='Name of CVS file', required=True)
 parser.add_argument('--password', help='BIG-IP Password')
 args = vars(parser.parse_args())
 
 BIGIP_URL_BASE = 'https://%s/mgmt/tm' % args['host']
-FILENAME = './static_routes_%s.csv' % (str(args['host']).replace('.', '_'))
 
 # Setup Password
 if args['password'] != None:
@@ -30,17 +30,26 @@ bigip.verify = False
 bigip.headers.update({'Content-Type' : 'application/json'})
 bigip.auth = (args['username'], password)
 
+routes = []
+# Read in CVS file
 try:
-    response = bigip.get('%s/net/route/' % (BIGIP_URL_BASE))
-    if response.status_code == 200:
-        routes = response.json()['items']
+    f = open(args['file'], 'r')
+    for line in f:
+        # Remove newline characters and whitespace characters
+        line = str(line).replace('\n','').replace('\r','').replace(' ', '')
+        fields = line.split(',')
+        routes.append({'name': fields[0], 'network': fields[1], 'gw': fields[2]})
+except Exception as e:
+    print('Encountered problem reading file: %s' % args['file'])
+    sys.exit(1)
 
-        # Open csv file to write values
-        f = open(FILENAME, 'w')
-        for route in routes:
-            f.write('%s,%s,%s\n' % (route['name'], route['network'], route['gw']))
-        f.close()
-    else:
-        print('Could not get static routes')
+# Push Routes to BigIP
+try:
+    for route in routes:
+        response = bigip.post('%s/net/route/' % (BIGIP_URL_BASE), data=json.dumps(route))
+        if response.status_code == 200:
+            print('Success creating route %s' % str(route))
+        else:
+            print('Failed creating route %s with message %s' % (str(route), response.text))
 except Exception as e:
     print('Communication failure with BigIP: %s' % args['host'])
